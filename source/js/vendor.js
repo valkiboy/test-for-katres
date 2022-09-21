e5cbfa0e0e438582e0de6c18ed9924ead364eedd
valkiboy <val_kik@mail.ru>
2022-09-21 15:20:39 +0300

    Добавляет изображения, подгатавливает шрифт, цвет

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,16 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/swiper';
+
+const swiper = new Swiper('.intro__swiper', {
+
+  // Стрелки
+  navigation: {
+    nextEl: '.intro__button-next',
+    prevEl: '.intro__button-prev',
+  },
+
+  // Отключаем скрол на ПК
+  // simulateTouch: false,
+
+});
+
